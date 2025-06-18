14931ce7c7a0e6aef60267127cf91b9d1c291f03
louis lopez <louislopez@louiss-MacBook-Pro.local>
2025-06-18 14:15:40 -0700

    added lad2

---
+++ lad2.py
@@ -0,0 +1,16 @@
+# lad 2 python profile
+
+## step 1
+neame = "louis lopez narvaez"
+
+## step 2
+age = 17
+
+## stepe 3
+used_python = False 
+
+## step 4
+hobbies = ["gaming","watching amine","go out to the movies with friensds"]
+
+##  step 5
+favorite_things = {"amine":"one piece","food":"wingstop","color":"red",}
