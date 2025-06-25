4b3987f4dfb400a0a43a1f4b5a9815d94600ce9b
louis lopez <louislopez@louiss-MacBook-Pro.local>
2025-06-25 16:07:10 -0700

    added lad3

---
+++ lad3.py
@@ -0,0 +1,37 @@
+#lad3
+
+#task 1: working with strings
+food = "wingstop"
+print(food[0:3])
+print(food[-3:-1])
+first_last= food[0]+ food[-1]
+print(first_last)
+food_list= food.split()
+print(food_list)
+joined_food = "  ".join(food_list)
+print(joined_food)
+
+#task 2 working with lists
+number_list = [1, 7675647456, 32, -5, 0, 234]
+number_list.append(67)
+print(number_list)
+number_list.insert(3, 1000)
+print(number_list)
+number_list.pop()
+print(number_list)
+number_list.remove(number_list[1])
+print(number_list)
+print(number_list[0:3])
+print(number_list[-3:-1])
+first_last= number_list[0] + number_list[-1]
+print(first_last)
+
+#task 3 working with keys and values
+books= {"kazu kibuishi":"amulet", "michelle zauner":"H mart", "tommy orange":"there there", "angie thomas":"the hate u give"}
+print(books.keys())
+print(books.values())
+print(books.get("kazu kibuishi"))
+books.pop("tommy orange")
+print(books)
+del books["michelle zauner"]
+print(books)
